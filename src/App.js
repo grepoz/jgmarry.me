37804261685de5165e191ffce04fbabff3428c7c
grepoz <grzegorz.pozorski@gmail.com>
2022-12-26 14:46:43 +0100

    add db connection funcs & refactor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import logo from './img/logo.svg';
 import './App.css';
-import { initializeApp } from "firebase/app";
+import getFamilyMembers from './seed-database/seed-database';
+
 
 function App() {
-
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyBdipdxNg8bs_Z9mOuGWZJD-ZFghkD_K1o",
-        authDomain: "wedding-41b3e.firebaseapp.com",
-        projectId: "wedding-41b3e",
-        storageBucket: "wedding-41b3e.appspot.com",
-        messagingSenderId: "209320284654",
-        appId: "1:209320284654:web:9e7a2a3de26b7ed1517eff"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
     return (
         <div>
+            <div>{getFamilyMembers("+de,}/")}</div>
             <div id="container">
 
                 <div id="content">
