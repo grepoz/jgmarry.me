43cc21bea15c27d877d00fce015405247b72de99
grepoz <grzegorz.pozorski@gmail.com>
2023-01-03 00:57:28 +0100

    added token generation

--- src/backend/python-scripts/generate_JWT_token_for_guests.py
+++ src/backend/python-scripts/generate_JWT_token_for_guests.py
@@ -1,13 +1,27 @@
 import firebase_admin
 from firebase_admin import auth
+import json
+from config import SERVICE_ACCOUNT_ID, PASSWORDS_FILEPATH
 
-options = {
-    'serviceAccountId': 'firebase-adminsdk-6z854@wedding-41b3e.iam.gserviceaccount.com',
-}
-firebase_admin.initialize_app(options=options)
 
-uid = 'some-uid'
+# this script creates JWT token, which provide extra security for users
+if __name__ == "__main__":
+    options = {
+        "serviceAccountId": SERVICE_ACCOUNT_ID
+    }
 
-# https://cloud.google.com/docs/authentication/getting-started  TODO
-custom_token = auth.create_custom_token(uid)
-print(custom_token)
+    firebase_admin.initialize_app(options=options)
+
+    with open(PASSWORDS_FILEPATH, 'r') as file:
+        passwords = file.read().splitlines()
+
+    tokens = []
+    for password in passwords:
+        if password != "":
+            custom_token = auth.create_custom_token(password)
+            tokens.append({password: custom_token.decode('utf-8')})
+
+    with open("tokens.json", "w") as file:
+        json.dump(tokens, file)
+
+# TODO: revoke credentials when not needed: 'gcloud auth application-default revoke'
