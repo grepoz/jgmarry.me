74a9aa25777c2dcd02a95353d422d3680f09fef2
grepoz <grzegorz.pozorski@gmail.com>
2023-02-13 22:43:56 +0100

    change library to firebase-admin

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,52 +1,41 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, request
-import pyrebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 import json
+
+from models.family import Family
 
 from models.requestModels import LoginParams
 
-config = {
-  "apiKey": os.getenv('API_KEY'),
-  "authDomain": os.getenv('FIREBASE_AUTH_DOMAIN'),
-  "databaseURL": os.getenv('FIREBASE_DATABASE_URL'),
-  "storageBucket": os.getenv('FIREBASE_STORAGE_BUCKET'),
-}
-
 load_dotenv()
 
-firebase = pyrebase.initialize_app(config)
+cred = credentials.Certificate('service_account_key.json')
+
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 app.config['TESTING'] = True
 
 
 @app.post("/login")
 def login():
-    auth = firebase.auth()
-
-    email = os.getenv('MAIN_USER_EMAIL')
-    password = os.getenv('MAIN_USER_PASSWORD')
 
     family_password = LoginParams.parse_obj(request.json).familyPassword
 
-    user = auth.sign_in_with_email_and_password(email, password)
-    # what if user is unauthorized?
-
-    if user is not None:
-        db = firebase.database()
-        families = db.child("families").get()
-        for family in families:
-            if family == family_password:
-                return json.dumps({'success': True, 'family': family}), 200, {'ContentType': 'application/json'}
+    ref = db.reference("families")
+    families: [Family] = ref.get()
+    # for family.password in families:
+    #     if family == family_password:
+    #         return json.dumps({'success': True, 'family': family}), 200, {'ContentType': 'application/json'}
 
     return json.dumps({'success': False}), 401, {'ContentType': 'application/json'}
 
 
 @app.post("/signupFamily")
 def register_family():
-    db = firebase.database()
-
-    db.child("users").child("Morty")
 
     return ""
 
