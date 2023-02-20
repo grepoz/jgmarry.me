ae33f6118bfc075978a27e5e48328861db106f39
grepoz <grzegorz.pozorski@gmail.com>
2023-02-20 22:31:38 +0100

    fix routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
 			<ul className="myUL">
 				<li><NavLink className="active" to="/">Home</NavLink ></li>
 				<li><NavLink  to="/about">About</NavLink ></li>
-				<li><NavLink  to="/register">Login</NavLink ></li>
+				<li><NavLink  to="/login">Login</NavLink ></li>
 			</ul>
 		</div>
 	);
