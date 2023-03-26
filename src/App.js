07e1cd968e239ae4a130290a872db86bf816d4fc
Julia <julia.naczke007@gmail.com>
2023-03-26 13:54:44 +0200

    delete error page

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Home from "./pages/home.js";
 import About from "./pages/about";
 import Info from "./pages/info";
 import SignupFamily from './pages/signupFamily.js';
-import ErrorPage from './pages/errorPage.js'
+import Login from "pages/login";
 
 function App() {
     const appName = configData.APP_NAME;
     
     return (
         <BrowserRouter>
-                <Header />
+            <Header />
             <main className="main">
                 <Home />   
                 <About /> 
                 <Info />
+                <Login />
                 <Routes>
                     <Route basename={`/${appName}`} path="signupFamily" element={<SignupFamily />} />
-                    <Route basename={`/${appName}`} path="*" element={<ErrorPage />} />
                 </Routes>
             </main>
             <footer></footer>
