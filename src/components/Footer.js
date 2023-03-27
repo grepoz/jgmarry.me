1a02cf4a29ddde4d89ea9e62b3ff0411009ed9c2
grepoz <grzegorz.pozorski@gmail.com>
2023-03-27 23:31:20 +0200

    add duplicated reading env var that set insta url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,11 @@ import "../styles/Footer.css"
 
 function Footer() {
 	
+	let instagramUrl = process.env.REACT_APP_INSTAGRAM_URL; 
+    if (!process.env.REACT_APP_INSTAGRAM_URL){
+        instagramUrl = "#instagram";
+    }
+
 	return (
 		<footer className="footer">
 			<div className="footer__container container">
@@ -26,10 +31,10 @@ function Footer() {
             		<a href="https://www.facebook.com/profile.php?id=100008116397277" className="footer__social-link" target="_blank" rel="noreferrer">
                 		<i className="uil uil-facebook-f"></i>
            		 	</a>
-            		<a href="#instagram" className="footer__social-link" target="_blank">
+            		<a href={instagramUrl} className="footer__social-link" target="_blank" rel="noreferrer">
                 		<i className="uil uil-instagram"></i>
             		</a>
-            		<a href="#gmail" className="footer__social-link" target="_blank">
+            		<a href="https://accounts.google.com/b/0/AddMailService" className="footer__social-link" target="_blank" rel="noreferrer">
                 		<i className="uil uil-envelope"></i>
             		</a>
         		</div>
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 const Social = () => {
+
+    let instagramUrl = process.env.REACT_APP_INSTAGRAM_URL; 
+    if (!process.env.REACT_APP_INSTAGRAM_URL){
+        instagramUrl = "#instagram";
+    }
+
     return (
         <div className="home__social">
             <a href="https://www.facebook.com/profile.php?id=100008116397277" className="home__social-icon" target="_blank" rel="noreferrer">
                 <i className="uil uil-facebook-f"></i>
             </a>
-            <a href="#instagram" className="home__social-icon" target="_blank">
+            <a href={instagramUrl} className="home__social-icon" target="_blank" rel="noreferrer">
                 <i className="uil uil-instagram"></i>
             </a>
-            <a href="#gmail" className="home__social-icon" target="_blank">
+            <a href="https://accounts.google.com/b/0/AddMailService" className="home__social-icon" target="_blank" rel="noreferrer" >
                 <i className="uil uil-envelope"></i>
             </a>
         </div>
