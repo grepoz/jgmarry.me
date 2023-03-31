69948a46af21643617af7fd45d4f63f22f3aa579
grepoz <grzegorz.pozorski@gmail.com>
2023-03-31 11:19:49 +0200

    simplify and clear some issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
